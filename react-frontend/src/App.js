74bdc7ebbedb2a8cfa630eba4f06f2517ce34380
Abhay Jolly <abhayjolly@Abhays-MacBook-Pro.local>
2022-05-02 19:18:04 -0700

    Added Display feature

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent/>
+    <div >
+       <Router>
+        
+              <HeaderComponent/>
+              <div className="container">
+                  <Routes> http://localhost:3000/
+                      <Route path = "/" element = {<ListEmployeeComponent/>}></Route>
+                      <Route path = "/employees" element = {<ListEmployeeComponent/>}></Route>
+                  </Routes>
+              </div>
+              <FooterComponent/>
+          
+       </Router>
     </div>
   );
 }
